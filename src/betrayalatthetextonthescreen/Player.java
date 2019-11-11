d07cd1bf9c337b706dfc91b3392be67789fb8140
Pippy <Pippy@132.238.241.114>
2019-11-11 09:23:03 -0500

    Added player class code

--- src/betrayalatthetextonthescreen/Player.java
+++ src/betrayalatthetextonthescreen/Player.java
@@ -1,6 +1,9 @@
 
 package betrayalatthetextonthescreen;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Player Class <br>
  *    Each instance is a player <br>
@@ -12,13 +15,69 @@ package betrayalatthetextonthescreen;
  * @author pippy
  */
 public class Player {
-    static final String DEFAULT_PLAYER_NAME = "I am confused";
-    String playerName;
+    static final String DEFAULT_PLAYER_NAME = "I'm a default";
+    static final int MAX_INVENTORY_SIZE = 10;
+    private String playerName;
+    private int playerLocation;
+    private List<String> playerInventory;
     
-    Player (String name) {
-        System.out.println("Player Class constructor called");
+    Player (String name) 
+    {
+        playerName = name;
+        playerLocation = 0;
+        playerInventory = new ArrayList<String>();
     }
     
+    public String getPlayerName()
+    {
+        return this.playerName;
+    }
+    
+    public int getPlayerLocation()
+    {
+        return this.playerLocation;
+    }
+    
+    public void setPlayerLocation(int room)
+    {
+        this.playerLocation = room;
+    }
+    
+    public List<String> getPlayerInventory()
+    {
+        return this.playerInventory;
+    }
+    
+    public void clearPlayerInventory()
+    {
+        this.playerInventory.clear();
+    }
+    
+    public boolean ifPlayerHasItem (String item)
+    {
+        boolean playerHasItem = false;
+        for (String playerInventoryItem : this.playerInventory) 
+        {
+            if (playerInventoryItem.equals(item)) 
+            {
+                playerHasItem = true;
+            }
+        }
+        return playerHasItem;
+    }
+    
+    public String addInventoryItem(String item)
+    {
+        if(this.playerInventory.size() < MAX_INVENTORY_SIZE)
+        {
+            this.playerInventory.add(item);
+            return item + " has been added to your inventory.";
+        }
+        else
+        {
+            return "Your inventory is full.  You'll have to drop something first.";
+        }
+    }
     // other methods to follow
     
 }
